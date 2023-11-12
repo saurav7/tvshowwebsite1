157a35dd53a47b07077a50c0e67829b614c5e12d
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 22:06:42 +1100

    create call back function for handle click when the button is cicked

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
 function SearchBar() {
+
+    const history = useHistory()
+
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        if (searchTerm) {
+            history.push(`/search?q=${searchTerm}`)
+        }
+    }
+
+    
+
     return (
         <div className="SearchBar">
-            <input type="text" placeholder="Search TV Shows" className="search"/>
-            <button className="search-button">Search</button>
+            <input type="text" placeholder="Search TV Shows" className="search" value={searchTerm} />
+            <button className="search-button" onClick={handleClick}>Search</button>
         </div>
     )
 }
