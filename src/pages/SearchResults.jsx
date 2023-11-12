a6c2d64963048de66a0910605a2cd975bd5a446b
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 23:17:25 +1100

    get response from api and try to get data from each result

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,4 +1,33 @@
-function SearchResults(){
+import { useState, useEffect } from "react"
+import axios from "axios"
 
+function SearchResults(props) {
+
+    const [searchTerm, setSearchTerm] = useState("")
+    const [searchResult, setSearchResults] = useState([])
+
+    useEffect(() => {
+
+        const query = new URLSearchParams(props.location.search).get("q")
+
+        const fetchData = async () => {
+            setSearchResults([])
+
+            if (query) {
+                const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
+                const results = response.data;
+
+                results.forEach(({ show }) => {
+                    axios.get(show._links.self.href)
+                        .then(res => {
+                            setSearchResults(prevState => ([...prevState, res.data]))
+                            setSearchTerm(query)
+                        })
+                });
+            }
+        }
+        fetchData()
+    }, [props.location.search])
+    return
 }
 export default SearchResults
