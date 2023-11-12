7c10f48dc11e251e9e079ab40ef1ba9899506199
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 21:19:13 +1100

    create a card for each tv show with image name and button on it from the data passed as a prop from TvShows.jsx

--- src/components/TvShowCard.jsx
+++ src/components/TvShowCard.jsx
@@ -1,4 +1,18 @@
-function TvShowCard({tvShow}){
+function TvShowCard({ tvShow }) {
+    return (
+        <div className="tvShow-card">
+            <div className="show-img-title-button">
+                {/* Check if tvShow is not null before accessing its properties  */}
+                {tvShow && tvShow.image && tvShow.image.medium && (
+                    <img className="card-image" src={tvShow.image.medium} alt={tvShow.name} />
+                )}
+                {tvShow && tvShow.name && (
+                    <h1>{tvShow.name}</h1>
+                )}
 
+                <button className="show-info"> Show Info</button>
+            </div>
+        </div>
+    )
 }
 export default TvShowCard
