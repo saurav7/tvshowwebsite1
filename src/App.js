07d75bcb9bc25f022080d5afe3c119e963a75cee
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 20:39:27 +1100

    create router in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      App
+        <Router>
+          <Route exact path = "/" component = {Home}/>
+          <Route path = "/about" component = {About}/>
+        </Router>
     </div>
   );
 }
