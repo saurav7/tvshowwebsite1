ef4f34fc02691ded2c0e28a6745b413c7e28401b
Saurav <saurav.ronaldo7@gmail.com>
2023-11-13 08:03:24 +1100

    add logo to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min"
 import SearchBar from "./SearchBar"
+import logo from "../images/logo.png"
 
 function NavBar() {
     return (
         <nav className="NavBar">
             <div className="NavContent">
+                <NavLink className="NavLink" to='/tvshows'><img className="logo" src={logo}/></NavLink>
                 <NavLink className="NavLink" to="/" >Home</NavLink>
                 <NavLink className="NavLink" to="/about" >About</NavLink>
                 <NavLink className="NavLink" to='/tvshows'>TvShows</NavLink>
