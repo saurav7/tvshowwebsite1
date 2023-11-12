9e0786f6c4340bdfadb394689b50a35dcef41535
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 21:53:23 +1100

    create searchbar.jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+function SearchBar() {
+    return (
+        <div className="SearchBar">
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    )
+}
+export default SearchBar
