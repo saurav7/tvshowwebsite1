5fff188c22bf6dc0c6f2a1618c6db6e0d28f921b
Saurav <saurav.ronaldo7@gmail.com>
2023-11-12 23:47:20 +1100

    make some divs to show the details of that particular show after getting the state of that show

--- src/components/TvShowDetails.jsx
+++ src/components/TvShowDetails.jsx
@@ -1,9 +1,25 @@
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min"
 
-function TvShowDetails(){
+function TvShowDetails() {
 
     const { state } = useLocation()
-    console.log("State::::::",state)
+
+    return (
+        <div className="show-detail-container">
+            <div className="show-card">
+                <div className="show-details">
+
+                    <img src={state.image.medium} alt="" />
+                    <div className="img-title-genres-rating">
+                        <h1>Name: {state.name}</h1>
+                        <h1>Genres: {state.genres}</h1>
+                        <h1>Rating: {state.rating.average}</h1>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 
 }
 export default TvShowDetails
